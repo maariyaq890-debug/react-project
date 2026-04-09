32a544a9f7569970f3d0f7781cdf0ffba57b533d
maariyaq890 <maariyaq890@gmail.com>
2026-04-09 23:57:28 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Services from "./sections/Services";
+import Testimonials from "./sections/Testimonials";
+
+import Footer from "./sections/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Testimonials />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
